fbefb78444196224454b8a4de1e8afb87e75ab90
Wilson Neto <wilsonpsn@hotmail.com>
2020-08-23 01:15:52 -0300

    playing videos

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 const Button = ({ text }: ButtonProps) => {
 	return (
 		<div>
-			<button class="bg-pink-700 hover:bg-pink-500 text-white font-bold py-2 px-4">
+			<button className="bg-pink-700 hover:bg-pink-500 text-white font-bold py-2 px-4">
 				{text}
 			</button>
 		</div>
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../Components/Button'
 
 const Home = () => {
+	const [currentVideo, setCurrentVideo] = useState('')
+	function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
+		console.log()
+		const file = evt.target.files[0]
+		const objectUrl = URL.createObjectURL(file)
+		setCurrentVideo(objectUrl)
+	}
+
 	return (
 		<div>
+			<input
+				type="file"
+				name=""
+				accept="video/*"
+				onChange={handleChange}
+				id=""
+			/>
+			{currentVideo !== '' && (
+				<video controls autoPlay>
+					<source src={currentVideo} type="" />
+				</video>
+			)}
 			<Button text={'Botão exemplo'} />
 		</div>
 	)
